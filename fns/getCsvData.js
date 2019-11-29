47ae02f9328759974455b3882e13d43ad3e69410
josiahwiebe <josiah@collectif.co>
2019-11-29 11:17:33 -0600

    fix: update to support more line-endings, encoding

--- fns/getCsvData.js
+++ fns/getCsvData.js
@@ -1,10 +1,13 @@
 const { createReadStream } = require('fs')
 const csv = require('csv-parser')
+const iconv = require('iconv-lite')
 
 const getCsvData = async csvFile => {
   let results = new Array()
   return new Promise((resolve) => {
     createReadStream(csvFile)
+      .pipe(iconv.decodeStream('utf8'))
+      .pipe(iconv.encodeStream('utf8'))
       .pipe(csv())
       .on('data', data => results.push(data))
       .on('end', () => resolve(results))
--- fns/updateDescriptions.js
+++ fns/updateDescriptions.js
@@ -4,13 +4,15 @@ const exiftoolBin = require('dist-exiftool')
 const updateDescriptions = async (basePath, images, csvData) => {
   const ep = new exiftool.ExiftoolProcess(exiftoolBin)
   await ep.open()
+
   for await (const image of images) {
-    let imageId = image.split(' ')[0]
+    let imageId = image.split('.')[0]
     let rowMatch = await csvData.find(row => row.id === imageId)
     if (rowMatch) {
       await ep.writeMetadata(`${basePath}/${image}`, {
-        ImageDescription: rowMatch.description
-      })
+        Description: String(rowMatch.description).replace(/\n/g, '\r'),
+        Caption: String(rowMatch.description).replace(/\n/g, '\r'),
+      }, ['charset=UTF8', 'overwrite_original', 'E'])
     }
   }
   await ep.close()
