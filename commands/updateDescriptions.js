780ce0d527b546aee1b36bad30920826c14ae32a
josiahwiebe <josiah@collectif.co>
2024-10-29 16:17:48 -0500

    feat: use exiftool-vendored fixes #1

--- commands/updateDescriptions.js
+++ commands/updateDescriptions.js
@@ -1,4 +1,4 @@
-import exiftool from 'node-exiftool'
+import { exiftool } from 'exiftool-vendored'
 import yoctoSpinner from 'yocto-spinner'
 import { getCsvData } from '../fns/getCsvData.js'
 import { getImages } from '../fns/getFiles.js'
@@ -10,4 +10,31 @@ export default async (options) => {
 
   spinner.text = 'Updating EXIF descriptions...'
   await Promise.all(
+    images.map(async (image) => {
+      let imageId = image.split('.')[0]
+      let rowMatch = await csvData.find((row) => row[options.id] === imageId)
+      if (rowMatch) {
+        const caption = String(rowMatch[options.description]).replace(/\n/g, '\r')
+        await exiftool.write(
+          `${options.source}/${image}`,
+          {
+            Description: caption,
+            Caption: caption,
+            LocalCaption: caption,
+          },
+          {
+            writeArgs: ['-overwrite_original', '-codedcharacterset=utf8', '-charset=UTF8', '-E'],
+          }
+        )
+      } else {
+        console.error(`No match found for ${imageId}`)
+      }
+    })
+  )
+    .then(() => {
+      spinner.success('EXIF descriptions updated.')
+    })
+    .finally(() => {
+      exiftool.end()
+    })
 }
